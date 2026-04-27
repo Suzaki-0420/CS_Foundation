efdce4eb0900f4490c45b7f90533c93633cc5ff6
Suzaki-0420 <chihoko.suzaki@wemex.com>
2026-04-27 16:57:08 +0900

    finished Ex_02_03

--- Ex_02_03/Program.cs
+++ Ex_02_03/Program.cs
@@ -1,12 +1,15 @@
-﻿namespace Ex_02_03;
+﻿using System.Data;
+
+namespace Ex_02_03;
 
 static class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("文字列を入力してください->");
-        int a = Console.ReadLine();//変数valueに入力された値を代入
-        Console.WriteLine($"5を加算した結果は{a+5}です");
-        Console.WriteLine($"5を減算した結果は{a-5}です");
+        Console.Write("数値を入力してください->");
+        string a = Console.ReadLine();//変数valueに入力された値を代入
+        int b =int.Parse(a);
+        Console.WriteLine($"5を加算した結果は{b+5}です");
+        Console.WriteLine($"5を減算した結果は{b-5}です");
     }
 }
