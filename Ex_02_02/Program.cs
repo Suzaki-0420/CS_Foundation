d276c65e0a4774dba6d68b73a137f19f1bc3d7ce
Suzaki-0420 <chihoko.suzaki@wemex.com>
2026-04-27 15:37:02 +0900

    Ex_02_01

---
+++ Ex_02_02/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Ex_02_02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -4,6 +4,10 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int a=5;
+        int b=2;
+        float c=(float)a/b;
+        System.Console.Write("c=");//2.50
+        System.Console.WriteLine(c);
     }
 }
