53a80dcf5d9507da9d86eea8e7083bab2a6b8e3f
Suzaki-0420 <chihoko.suzaki@wemex.com>
2026-04-28 14:06:45 +0900

    finished Ex_02_12

---
+++ Ex_02_12/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex_02_12;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("値を入力してください->");
+        int num=int.Parse(System.Console.ReadLine());
+        int answer=Mul3(num);
+        Console.WriteLine(answer);
+
+        int Mul3(int x)
+        {
+            return x*3;
+        }
+    }
+}
