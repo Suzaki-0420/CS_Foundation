79dd286f3ad2d37bb0cac146140d4c5c54865ccb
Suzaki-0420 <chihoko.suzaki@wemex.com>
2026-04-28 15:04:46 +0900

    finished Ex_03_01

--- Ex_03_01/Program.cs
+++ Ex_03_01/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Ex_03_01;
+﻿using System.Net.ServerSentEvents;
+
+namespace Ex_03_01;
 
 static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Item item=new();
+        Console.Write("商品番号を入力してください->");
+        item.itemNo=int.Parse(System.Console.ReadLine());
+
+        Console.Write("商品名を入力してください->");
+        item.itemName=System.Console.ReadLine();
+
+        Console.Write("単価を入力してください->");
+        item.price=int.Parse(System.Console.ReadLine());
+
+        Console.WriteLine($"商品番号 = {item.itemNo}");
+        Console.WriteLine($"商品名 = {item.itemName}");
+        Console.WriteLine($"単価 = {item.price}");
     }
 }
