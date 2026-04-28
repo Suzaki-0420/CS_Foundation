920fb5281b2de1d1877a5c8de7a981ee53abe837
Suzaki-0420 <chihoko.suzaki@wemex.com>
2026-04-28 15:57:32 +0900

    start Ex_03_02 to Ex_03_04

---
+++ Ex_03_02/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Ex_03_02;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -1,17 +1,13 @@
-﻿string day = "本日";
-string wether = "晴れ";
-// 可変数の引数を渡せる
-StringConcatenation(day, "は", wether, "です。");
-/// <summary>
-/// 引数の文字列を結合して表示する
-/// </summary>
-/// <param name="strings">結合する文字列</param>
-static void StringConcatenation(params string[] strings)
+﻿namespace Example;
+
+
+static class Program
 {
-    var result = "";
-    for (int i = 0; i < strings.Length; i++)
+    static void Main(string[] args)
     {
-        result += strings[i];
+        Console.Write("値1を入力してください->");
+        string value1 = Console.ReadLine()!;
+        Console.Write("値2を入力してください->");
+        int value2 = int.Parse(Console.ReadLine()!);
     }
-    Console.WriteLine(result);
 }
