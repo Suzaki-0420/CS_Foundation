97970346e442fad71c021e46c63fbf3b6a01b6d0
Suzaki-0420 <chihoko.suzaki@wemex.com>
2026-05-01 11:40:17 +0900

    finished Ex_03_11

---
+++ Ex_03_11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex_03_11;
+using Ex_03_11.Modules;
+static class Program
+{
+    static void Main(string[] args)
+    {
+        var mouse = new Mouse();
+        var flashmemory = new FlashMemory();
+        var display = new Display();
+
+        flashmemory.SendData();
+        flashmemory.ReciveData();
+        mouse.SendData();
+        display.ReciveData();
+    }
+}
