e5ca11f8a95442c008887e42dabc11dabe15a718
Suzaki-0420 <chihoko.suzaki@wemex.com>
2026-05-01 15:24:44 +0900

    start Ex_03_16

---
+++ Ex_03_16/InputParameterLessThanZeroException.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_03_16
+{
+    public class InputParameterLessThanZeroException
+    {
+        
+    }
+}
---
+++ Ex_03_16/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex_03_16;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        var calculator = new Calculator();
+
+        Console.WriteLine("値1を入力してください->");
+        int x=int.Parse(System.Console.ReadLine());
+
+        Console.WriteLine("値2を入力してください->");
+        int y=int.Parse(System.Console.ReadLine());
+
+        try
+        {
+            int div=calculator.Div(x,y);
+            Console.WriteLine($"値1 / 値2 = {div}");
+        }
+        catch
+        {
+            Console.WriteLine("例外:Attempted to divide by zero.");
+        }
+    }
+}
